33ed1760d7a41c2b880de212164227fb815bfab2
roylaurie <roy.laurie.pub@sourcetrait.com>
2025-05-30 12:52:08 -0700

    doc fix

--- packages/srctrait-common-chronox/src/parse/date.rs
+++ packages/srctrait-common-chronox/src/parse/date.rs
@@ -1,30 +1,30 @@
 //! Extended traits for parsing chrono dates
 
-use std::borrow::Cow;
-use chrono::{DateTime, Datelike, NaiveDate, NaiveDateTime, TimeZone};
 use crate::*;
+use chrono::{DateTime, Datelike, NaiveDate, NaiveDateTime, TimeZone};
+use std::borrow::Cow;
 
 /// Parse a date string relative to this one, with year and month
 /// being optional.
 ///
 /// Supports variants of Y-m-d and Y/m/D.
 ///
-/// Implemented for chrono::{NaiveDate, NaiveDateTime, and DateTime<TZ>}.
+/// Implemented for `chrono::{NaiveDate, NaiveDateTime, and DateTime<TZ>}`.
 pub trait DateRelativeParsing
 where
-    Self: Datelike
+    Self: Datelike,
 {
     /// Parse a date string relative to this one, with year and month
     /// being optional.
     ///
     /// Supports variants of Y-m-d and Y/m/D.
     fn parse_relative_date(&self, date: &str) -> Option<NaiveDate> {
-        const FORMATS: [(&'static str, Option<fn(i32,u32,&str) -> String>); 5] = [
+        const FORMATS: [(&'static str, Option<fn(i32, u32, &str) -> String>); 5] = [
             (FMT_YMD_DASH, None),
             (FMT_YMD_SLASH, None),
-            (FMT_YMD_DASH, Some(|y,_,s| format!("{y}-{s}"))), // m-d
-            (FMT_YMD_SLASH, Some(|y,_,s| format!("{y}/{s}"))), // m/d
-            (FMT_YMD_DASH, Some(|y,m,s| format!("{y}-{m}-{s}"))), // d
+            (FMT_YMD_DASH, Some(|y, _, s| format!("{y}-{s}"))), // m-d
+            (FMT_YMD_SLASH, Some(|y, _, s| format!("{y}/{s}"))), // m/d
+            (FMT_YMD_DASH, Some(|y, m, s| format!("{y}-{m}-{s}"))), // d
         ];
 
         //let now = Local::now().naive_local();
@@ -50,8 +50,8 @@ impl<TZ: TimeZone> DateRelativeParsing for DateTime<TZ> {}
 
 #[cfg(test)]
 mod tests {
-    use chrono::{Local, Utc};
     use super::*;
+    use chrono::{Local, Utc};
 
     #[test]
     fn test_local_relative() {
@@ -59,23 +59,28 @@ mod tests {
 
         assert_eq!(
             NaiveDate::from_ymd_opt(2025, 3, 2),
-            now.parse_relative_date("2025-03-02"));
+            now.parse_relative_date("2025-03-02")
+        );
 
         assert_eq!(
             NaiveDate::from_ymd_opt(2025, 3, 2),
-            now.parse_relative_date("2025/3/2"));
+            now.parse_relative_date("2025/3/2")
+        );
 
         assert_eq!(
             NaiveDate::from_ymd_opt(now.year(), 4, 5),
-            now.parse_relative_date("04-05"));
+            now.parse_relative_date("04-05")
+        );
 
         assert_eq!(
             NaiveDate::from_ymd_opt(now.year(), 4, 5),
-            now.parse_relative_date("4/5"));
+            now.parse_relative_date("4/5")
+        );
 
         assert_eq!(
             NaiveDate::from_ymd_opt(now.year(), now.month(), 5),
-            now.parse_relative_date("5"));
+            now.parse_relative_date("5")
+        );
     }
 
     #[test]
@@ -84,6 +89,7 @@ mod tests {
 
         assert_eq!(
             NaiveDate::from_ymd_opt(now.year(), 4, 5),
-            now.parse_relative_date("04-05"));
+            now.parse_relative_date("04-05")
+        );
     }
 }
